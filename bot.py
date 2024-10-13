762cbd18dba0e46a8f87527714ee1156d8fd6199
ADmolnie <loma890@mail.ru>
2024-10-13 16:23:49 +0500

    add files

---
+++ bot.py
@@ -0,0 +1,34 @@
+import telebot
+
+API_TOKEN = '7702403200:AAHA7_1HpF3Et0akp_q3g_RumYTHSUmX2pU'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+bot.infinity_polling()
---
+++ config.py
@@ -0,0 +1,1 @@
+token = "7702403200:AAHA7_1HpF3Et0akp_q3g_RumYTHSUmX2pU"
